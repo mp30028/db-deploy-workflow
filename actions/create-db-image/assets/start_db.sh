0bcd601adcad857adec248d4ae08ba63af987f52
mp30028 <mp30028@gmail.com>
2023-02-19 10:49:14 +0000

    updated shell script to ensure container remains up

--- actions/create-db-image/assets/start_db.sh
+++ actions/create-db-image/assets/start_db.sh
@@ -34,7 +34,7 @@ echo "Image $image_name exists = $image_exists"
 if [ "$image_exists" = true ] 
 then
 	echo "Image $image_name will be removed"
-	sleep 2
+	sleep 1
 	docker rmi $image_name 
 fi
 
@@ -44,6 +44,6 @@ docker build --no-cache --pull -t $image_name ./
 #docker run --rm --name $container_name -d -p 3306:3306 $image_name
 
 #Create a new container and start it up 
-docker run --name $container_name -d -p 3307:3306 $image_name
+docker run --name $container_name -it -d -p 3307:3306 $image_name /bin/bash
 
 echo "NOTE THE DATABASE PORT IS 3307"
